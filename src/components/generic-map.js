73a41b720e8b2a13094d213be1ca22f8660ec4ed
goondrious <srs_34@hotmail.com>
2020-07-09 13:58:37 -0400

    Map - add generic map component and story

---
+++ src/components/generic-map.js
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react'
+import PropTypes from 'prop-types'
+
+import DeckGL, { FlyToInterpolator, MapView } from 'deck.gl'
+import { StaticMap } from 'react-map-gl'
+
+import { AppContext } from '../context'
+
+
+const MAP_VIEW = new MapView({ repeat: true });
+
+const INIT_VIEW_STATE = {
+  pitch: 0,
+  bearing: 0,
+  transitionDuration: 300,
+  transitionInterpolator: new FlyToInterpolator(),
+  latitude: 52,
+  longitude: -100,
+  zoom: 2.5,
+}
+
+const propTypes = { layers: PropTypes.array }
+const defaultProps = { layers: [] }
+
+// DeckGL react component
+const Map = ({ layers }) => {
+
+  const { isLightTheme } = useContext(AppContext)
+  
+  return (
+    <div style={{ height: '100%', width: '100%', position: 'absolute' }}>
+      <DeckGL
+        initialViewState={ INIT_VIEW_STATE } 
+        views={ MAP_VIEW }
+        layers={layers}
+        controller={ true }
+      >
+        <StaticMap 
+          mapboxApiAccessToken={ process.env.MAPBOX_ACCESS_TOKEN }
+          mapStyle={`mapbox://styles/mapbox/${isLightTheme ? 'light' : 'dark'}-v9`}
+        />
+      </DeckGL>
+    </div>
+  )
+}
+
+Map.propTypes = propTypes
+Map.defaultProps = defaultProps
+
+export default Map
