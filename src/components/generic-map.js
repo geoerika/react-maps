9fbf897082ce8c84b0309e8e964e900ee764d71c
Shane Stratton <srs_34@hotmail.com>
2020-07-17 10:36:20 -0400

    GenericMap - remove AppContext

--- src/components/generic-map.js
+++ src/components/generic-map.js
@@ -4,8 +4,6 @@ import PropTypes from 'prop-types'
 import DeckGL, { FlyToInterpolator, MapView } from 'deck.gl'
 import { StaticMap } from 'react-map-gl'
 
-import { AppContext } from '../context'
-
 
 const MAP_VIEW = new MapView({ repeat: true });
 
@@ -23,29 +21,21 @@ const propTypes = { layers: PropTypes.array }
 const defaultProps = { layers: [] }
 
 // DeckGL react component
-const Map = ({ layers }) => {
-
-  const { isLightTheme } = useContext(AppContext)
-  
-  return (
-    <div style={{ height: '100%', width: '100%', position: 'absolute' }}>
-      <DeckGL
-        initialViewState={ INIT_VIEW_STATE } 
-        views={ MAP_VIEW }
-        layers={layers}
-        controller={ true }
-        // NOTE: same structure as layer click
-        // onHover={d => console.log('----> map hover', d)}
-        // onClick={d => console.log('----> map click', d)}
-      >
-        <StaticMap 
-          mapboxApiAccessToken={ process.env.MAPBOX_ACCESS_TOKEN }
-          mapStyle={`mapbox://styles/mapbox/${isLightTheme ? 'light' : 'dark'}-v9`}
-        />
-      </DeckGL>
-    </div>
-  )
-}
+const Map = ({ layers }) => (
+  <div style={{ height: '100%', width: '100%', position: 'absolute' }}>
+    <DeckGL
+      initialViewState={ INIT_VIEW_STATE }
+      views={ MAP_VIEW }
+      layers={layers}
+      controller={ true }
+      // NOTE: same structure as layer click
+      // onHover={d => console.log('----> map hover', d)}
+      // onClick={d => console.log('----> map click', d)}
+    >
+      <StaticMap mapboxApiAccessToken={ process.env.MAPBOX_ACCESS_TOKEN } />
+    </DeckGL>
+  </div>
+)
 
 Map.propTypes = propTypes
 Map.defaultProps = defaultProps
--- src/datasets.js
+++ src/datasets.js
@@ -1,7 +1,10 @@
+// NOTE: predefine dataset meta & metric keys to streamline
+// grouping, filtering
+// dynamic visualizations (a la Kepler), e.g. radius based on visits
 export const reportWI = {
   META_FIELDS: [
     'address_city', 'address_country', 'address_label', 'address_line1', 'address_line2', 'address_postalcode', 'address_region', 'address_unit',
-    'category', 'chain_id', 'date_type', 'end_date', 'start_date', 'repeat_id', 'lat', 'lon', 'name', 'poi_id', 'report_id', 'time_zone', 'type', 
+    'category', 'chain_id', 'date_type', 'end_date', 'start_date', 'repeat_id', 'lat', 'lon', 'name', 'poi_id', 'report_id', 'time_zone', 'type',
   ],
   DATA_FIELDS: [
     'repeat_type', 'repeat_visitors', 'repeat_visitors_hh', 'repeat_visits', 'report_id', 'unique_hh', 'unique_visitors', 'unique_visitors_dow',
