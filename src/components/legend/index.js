80c698dbb69d9fa4ac6d404a51496efb560be3ce
goondrious <srs_34@hotmail.com>
2020-07-20 12:18:32 -0400

    Legend - use flexbox styling

--- src/components/legend/index.js
+++ src/components/legend/index.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 
 
 const LegendContainer = styled.div`
+  display: flex;
   position: absolute;
   z-index: 9999;
   background-color: rgba(255,255,255,0.9);
@@ -12,26 +13,29 @@ const LegendContainer = styled.div`
   ${props => props.position}
 `
 
+const LegendTextContainer = styled.div`
+  display: flex;
+  flex-grow: 4;
+  flex-direction: column;
+`
+
 const LegendText = styled.div`
-  float: left;
+  ${props => props.top ? 'flex-grow: 1;' : ''};
   margin-left: 1rem;
-  margin-bottom: 1rem;
   font-size: 1rem;
   color: black;
 `
 
+const LegendSymbolContainer = styled.div`
+  flex-grow: 1;
+  align-self: flex-start;
+`
+
 const LegendGradient = styled.div`
-  float: left;
   width: 15px;
   height: 80px;
   background-image: linear-gradient(rgb(21, 0, 255), rgb(1, 0, 70));
 `
-
-// height/width -> wrap
-
-// configurable
-// which corner
-// color
 
 const propTypes = {
   max: PropTypes.number,
@@ -50,9 +54,11 @@ const defaultProps = {
 const Legend = ({ max, min, label, position }) => <>
   { max !== undefined && min !== undefined && (
     <LegendContainer position={`${position.split('-')[0]}: 1rem; ${position.split('-')[1]}: 1rem;`}>
-      <LegendGradient />
-      <LegendText>{max.toLocaleString()} {label}</LegendText>
-      <LegendText>{min.toLocaleString()} {label}</LegendText>
+      <LegendSymbolContainer><LegendGradient /></LegendSymbolContainer>
+      <LegendTextContainer>
+        <LegendText top>{max.toLocaleString()} {label}</LegendText>
+        <LegendText>{min.toLocaleString()} {label}</LegendText>
+      </LegendTextContainer>
     </LegendContainer>
   )}
 </>
