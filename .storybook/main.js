ff5b332aed43701bfc7401d8258aff7c97e9c0e3
Erika Szabo <erika.szabo.geo@gmail.com>
2020-08-26 08:55:55 -0400

    Storybook - fix webpack issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,12 @@
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-};
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+        type: 'javascript/auto',
+        test: /\.mjs$/,
+        use: []
+    })
+    return config
+  }
+}
