03b81a9d6f9303953a34d3ade584c305d2107e20
Erika Szabo <erika.szabo.geo@gmail.com>
2020-08-28 08:57:00 -0400

    devops - fix turf issues for github workflows

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,4 @@
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  webpackFinal: async (config, { configType }) => {
-    config.module.rules.push({
-        type: 'javascript/auto',
-        test: /\.mjs$/,
-        use: []
-    })
-    return config
-  }
 }
