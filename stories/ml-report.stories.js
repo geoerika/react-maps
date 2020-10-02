098e4af064c1f30f1d7fa12c697ec5ba66b22b8c
goondrious <srs_34@hotmail.com>
2020-10-02 14:01:15 -0400

    MLReport - data and stories

---
+++ stories/ml-report.stories.js
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react'
+import { storiesOf } from '@storybook/react'
+import { MLReportMap } from '../src'
+import vwiJson from './data/locus-ml-vwi.json'
+import { object } from 'prop-types'
+
+storiesOf('Locus ML Report', module)
+  .add('VWI - basic', () => (
+    <MLReportMap reportData={vwiJson} />
+  ))
+  .add('VWI - zoom to point', () => {
+    const [centerMap, setCenterMap] = useState()
+    const [highlightId, setHighlightId] = useState()
+    const zoom = () => {
+      setCenterMap({ longitude: vwiJson[0].lon, latitude: vwiJson[0].lat, zoom: 15 })
+      setHighlightId(vwiJson[0].poi_id)
+    }
+    return (
+      <div>
+        <button onClick={zoom}>Zoom</button>
+        <MLReportMap
+          reportData={vwiJson}
+          centerMap={centerMap}
+          highlightId={highlightId}
+          getTooltip={o => o.object?.name}
+        />
+      </div>
+    )
+  })
