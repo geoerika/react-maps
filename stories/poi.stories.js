07a9510d7315bb25caca0ca2afcbca79db948894
Erika Szabo <erika.szabo.geo@gmail.com>
2020-11-05 20:29:10 -0500

    Stories/poi - add edit instructions

--- stories/poi.stories.js
+++ stories/poi.stories.js
@@ -4,7 +4,19 @@ import { storiesOf } from '@storybook/react'
 import { POIMap, POIMapActivePOI } from '../src'
 import POIsRadii from './data/pois-radii'
 import POIsRadiiTo from './data/pois-radiito'
-import POIsPolygonsVan from './data/pois-polygons-van'
+import POIsPolygonsVanData from './data/pois-polygons-van'
+import { styled, setup } from 'goober'
+
+
+setup(React.createElement)
+
+const Text = styled('div')`
+  color: red;
+`
+
+// create GeoJSON format for polygon data
+const POIsPolygonsVan = POIsPolygonsVanData.map((polygon) =>
+  ({ ...polygon, geometry: JSON.parse(polygon.properties.polygon_json) }))
 
 storiesOf('POI Map', module)
   .add('Point POIs - icons with POIIcon layer', () => (
@@ -32,28 +44,37 @@ storiesOf('POI Map', module)
     <POIMap POIData={ POIsPolygonsVan } layerArray={ ['POIGeoJson'] }/>
   ))
   .add('Edit Point POIs - POIEditDraw layer', () => (
-    <POIMap
-      POIData={ [POIsRadiiTo[0]] }
-      layerArray={ ['POIEditDraw'] }
-      mode={ 'poi-edit' }
-      controller={{ doubleClickZoom: false }}
-    />
+    <div>
+      <Text>Click feature to start editing!</Text>
+      <POIMap
+        POIData={ [POIsRadiiTo[0]] }
+        layerArray={ ['POIEditDraw'] }
+        mode={ 'poi-edit' }
+        controller={{ doubleClickZoom: false }}
+      />
+    </div>
   ))
   .add('Edit Radius POIs - POIEditDraw layer', () => (
-    <POIMap
-      activePOI={ POIsRadiiTo[0] }
-      layerArray={ ['POIEditDraw'] }
-      mode={ 'poi-point-radius-edit' }
-      controller={{ doubleClickZoom: false }}
-    />
+    <div>
+      <Text>Click feature to start editing!</Text>
+      <POIMap
+        activePOI={ POIsRadiiTo[0] }
+        layerArray={ ['POIEditDraw'] }
+        mode={ 'poi-point-radius-edit' }
+        controller={{ doubleClickZoom: false }}
+      />
+    </div>
   ))
   .add('Edit Polygon POIs - POIEditDraw layer', () => (
-    <POIMap
-      POIData={ [POIsPolygonsVan[0]] }
-      layerArray={ ['POIEditDraw'] }
-      mode={ 'poi-edit' }
-      controller={{ doubleClickZoom: false }}
-    />
+    <div>
+      <Text>Click feature to start editing!</Text>
+      <POIMap
+        POIData={ [POIsPolygonsVan[0]] }
+        layerArray={ ['POIEditDraw'] }
+        mode={ 'poi-edit' }
+        controller={{ doubleClickZoom: false }}
+      />
+    </div>
   ))
   .add('Draw Point POIs - POIEditDraw layer', () => (
     <POIMap 
