2c5acecd4de3e66bd3150c6420a108a86d84174d
Erika Szabo <erika.szabo.geo@gmail.com>
2021-02-22 20:50:28 -0500

    poi-category - add poi-category list file

---
+++ src/poi-category.js
@@ -0,0 +1,301 @@
+// TODO: fetch this list from DB
+
+// current hardcode fetch query:
+// SELECT poi_category_id as value,
+// name as key,
+// name as text
+// FROM
+// poi_category;
+
+export const poiCategory = [
+  {
+    value: 2,
+    key: 'Fast Food',
+    text: 'Fast Food',
+  },
+  {
+    value: 3,
+    key: 'Food',
+    text: 'Food',
+  },
+  {
+    value: 4,
+    key: 'Coffee & Tea',
+    text: 'Coffee & Tea',
+  },
+  {
+    value: 1,
+    key: 'Restaurant',
+    text: 'Restaurant',
+  },
+  {
+    value: 5,
+    key: 'Bakery',
+    text: 'Bakery',
+  },
+  {
+    value: 6,
+    key: 'Pastry',
+    text: 'Pastry',
+  },
+  {
+    value: 7,
+    key: 'Store',
+    text: 'Store',
+  },
+  {
+    value: 8,
+    key: 'Clothing Store',
+    text: 'Clothing Store',
+  },
+  {
+    value: 10,
+    key: 'Sport Clothing Store',
+    text: 'Sport Clothing Store',
+  },
+  {
+    value: 11,
+    key: 'Shoe Store',
+    text: 'Shoe Store',
+  },
+  {
+    value: 13,
+    key: 'Department Store',
+    text: 'Department Store',
+  },
+  {
+    value: 15,
+    key: 'Drug Store',
+    text: 'Drug Store',
+  },
+  {
+    value: 17,
+    key: 'Sport',
+    text: 'Sport',
+  },
+  {
+    value: 18,
+    key: 'Gym',
+    text: 'Gym',
+  },
+  {
+    value: 19,
+    key: 'Entertainment',
+    text: 'Entertainment',
+  },
+  {
+    value: 20,
+    key: 'Movie Theater',
+    text: 'Movie Theater',
+  },
+  {
+    value: 21,
+    key: 'Automotive',
+    text: 'Automotive',
+  },
+  {
+    value: 22,
+    key: 'Car Dealer',
+    text: 'Car Dealer',
+  },
+  {
+    value: 23,
+    key: 'Car Repair',
+    text: 'Car Repair',
+  },
+  {
+    value: 24,
+    key: 'Car Rental',
+    text: 'Car Rental',
+  },
+  {
+    value: 25,
+    key: 'Gas Station',
+    text: 'Gas Station',
+  },
+  {
+    value: 26,
+    key: 'Financial Services',
+    text: 'Financial Services',
+  },
+  {
+    value: 27,
+    key: 'Insurance',
+    text: 'Insurance',
+  },
+  {
+    value: 28,
+    key: 'Credit Union',
+    text: 'Credit Union',
+  },
+  {
+    value: 29,
+    key: 'Bank',
+    text: 'Bank',
+  },
+  {
+    value: 30,
+    key: 'Health',
+    text: 'Health',
+  },
+  {
+    value: 31,
+    key: 'Medical Clinics',
+    text: 'Medical Clinics',
+  },
+  {
+    value: 32,
+    key: 'Dental',
+    text: 'Dental',
+  },
+  {
+    value: 33,
+    key: 'Local Services',
+    text: 'Local Services',
+  },
+  {
+    value: 35,
+    key: 'Printing',
+    text: 'Printing',
+  },
+  {
+    value: 36,
+    key: 'Equipment Rental',
+    text: 'Equipment Rental',
+  },
+  {
+    value: 37,
+    key: 'Home Services',
+    text: 'Home Services',
+  },
+  {
+    value: 38,
+    key: 'Moving Company',
+    text: 'Moving Company',
+  },
+  {
+    value: 39,
+    key: 'Security',
+    text: 'Security',
+  },
+  {
+    value: 41,
+    key: 'Pets',
+    text: 'Pets',
+  },
+  {
+    value: 14,
+    key: 'Supermarket',
+    text: 'Supermarket',
+  },
+  {
+    value: 34,
+    key: 'It Services',
+    text: 'It Services',
+  },
+  {
+    value: 40,
+    key: 'Hotels And Travel',
+    text: 'Hotels And Travel',
+  },
+  {
+    value: 9,
+    key: 'Home Goods Store',
+    text: 'Home Goods Store',
+  },
+  {
+    value: 12,
+    key: 'Furniture And Electronics Store',
+    text: 'Furniture And Electronics Store',
+  },
+  {
+    value: 16,
+    key: 'Jewelry Store',
+    text: 'Jewelry Store',
+  },
+  {
+    value: 42,
+    key: 'Beauty And Spa',
+    text: 'Beauty And Spa',
+  },
+  {
+    value: 43,
+    key: 'Professional Services',
+    text: 'Professional Services',
+  },
+  {
+    value: 44,
+    key: 'Education',
+    text: 'Education',
+  },
+  {
+    value: 45,
+    key: 'Transit Station',
+    text: 'Transit Station',
+  },
+  {
+    value: 46,
+    key: 'Religious Organization',
+    text: 'Religious Organization',
+  },
+  {
+    value: 47,
+    key: 'Real Estate',
+    text: 'Real Estate',
+  },
+  {
+    value: 48,
+    key: 'Public and Government Services',
+    text: 'Public and Government Services',
+  },
+  {
+    value: 49,
+    key: 'Parks',
+    text: 'Parks',
+  },
+  {
+    value: 50,
+    key: 'Building',
+    text: 'Building',
+  },
+  {
+    value: 51,
+    key: 'Residence',
+    text: 'Residence',
+  },
+  {
+    value: 52,
+    key: 'Shopping Mall',
+    text: 'Shopping Mall',
+  },
+  {
+    value: 53,
+    key: 'Cannabis',
+    text: 'Cannabis',
+  },
+  {
+    value: 55,
+    key: 'country',
+    text: 'Country',
+  },
+  {
+    value: 56,
+    key: 'region',
+    text: 'Province',
+  },
+  {
+    value: 57,
+    key: 'place',
+    text: 'City',
+  },
+  {
+    value: 58,
+    key: 'fsa',
+    text: 'FSA',
+  },
+  {
+    value: 59,
+    key: 'postcode',
+    text: 'Postal Code',
+  },
+]
