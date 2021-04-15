8dda7309f98b74610a414ed1a22fddd2aaa5d095
Erika Szabo <erika.szabo.geo@gmail.com>
2021-04-15 17:49:42 -0400

    stories/geo-cohorts - create useGeoCohortData; add story for data-based colour fill and elevation in one map

--- stories/geo-cohorts.stories.js
+++ stories/geo-cohorts.stories.js
@@ -11,32 +11,37 @@ import { getPlaceGeo, FOApi } from './poi-manage'
 
 const mapboxApiAccessToken = process.env.MAPBOX_ACCESS_TOKEN
 
+const useGeoCohortData = () => {
+  const [geoCohortData, setGeoCohortData] = useState([])
+  useEffect(() => {
+    const getFSAsGeometry = async (dataArray) => {
+      let response = []
+      await Promise.all(dataArray.map(async elem => {
+        try {
+          await getPlaceGeo(FOApi)({ data: {
+            placeType: 'postcode',
+            country: 'CA',
+            postcode: elem._id.GeoCohortItem,
+          } }).then(result => {
+            response.push({
+              ...result,
+              properties: elem,
+            })
+          })
+        } catch (error) {
+          console.error(error)
+        }
+      }))
+      setGeoCohortData(response)
+    }
+    getFSAsGeometry(geoCohortJson)
+  }, [])
+  return geoCohortData
+}
+
 storiesOf('Geo-Cohort Map', module)
   .add('GeoCohortMap - basic', () => {
-    const [geoCohortData, setGeoCohortData] = useState([])
-    useEffect(() => {
-      const getFSAsGeometry = async (dataArray) => {
-        let response = []
-        await Promise.all(dataArray.map(async elem => {
-          try {
-            await getPlaceGeo(FOApi)({ data: {
-              placeType: 'postcode',
-              country: 'CA',
-              postcode: elem._id.GeoCohortItem,
-            } }).then(result => {
-              response.push({
-                ...result,
-                properties: elem,
-              })
-            })
-          } catch (error) {
-            console.error(error)
-          }
-        }))
-        setGeoCohortData(response)
-      }
-      getFSAsGeometry(geoCohortJson)
-    }, [])
+    const geoCohortData = useGeoCohortData()
     return (
       geoCohortData.length > 0 &&
         <GeoCohortMap
@@ -47,30 +52,7 @@ storiesOf('Geo-Cohort Map', module)
     )
   })
   .add('GeoCohortMap - colour fill based on impressions', () => {
-    const [geoCohortData, setGeoCohortData] = useState([])
-    useEffect(() => {
-      const getFSAsGeometry = async (dataArray) => {
-        let response = []
-        await Promise.all(dataArray.map(async elem => {
-          try {
-            await getPlaceGeo(FOApi)({ data: {
-              placeType: 'postcode',
-              country: 'CA',
-              postcode: elem._id.GeoCohortItem,
-            } }).then(result => {
-              response.push({
-                ...result,
-                properties: elem,
-              })
-            })
-          } catch (error) {
-            console.error(error)
-          }
-        }))
-        setGeoCohortData(response)
-      }
-      getFSAsGeometry(geoCohortJson)
-    }, [])
+    const geoCohortData = useGeoCohortData()
     return (
       geoCohortData.length > 0 &&
         <GeoCohortMap
@@ -82,35 +64,26 @@ storiesOf('Geo-Cohort Map', module)
     )
   })
   .add('GeoCohortMap - elevation based on impressions', () => {
-    const [geoCohortData, setGeoCohortData] = useState([])
-    useEffect(() => {
-      const getFSAsGeometry = async (dataArray) => {
-        let response = []
-        await Promise.all(dataArray.map(async elem => {
-          try {
-            await getPlaceGeo(FOApi)({ data: {
-              placeType: 'postcode',
-              country: 'CA',
-              postcode: elem._id.GeoCohortItem,
-            } }).then(result => {
-              response.push({
-                ...result,
-                properties: elem,
-              })
-            })
-          } catch (error) {
-            console.error(error)
-          }
-        }))
-        setGeoCohortData(response)
-      }
-      getFSAsGeometry(geoCohortJson)
-    }, [])
+    const geoCohortData = useGeoCohortData()
     return (
       geoCohortData.length > 0 &&
         <GeoCohortMap
           reportData={geoCohortData}
           getCursor={getCursor()}
+          elevationBasedOn={'Revenue'}
+          pitch={45}
+          mapboxApiAccessToken={mapboxApiAccessToken}
+        />
+    )
+  })
+  .add('GeoCohortMap - data-based colour fill and elevation', () => {
+    const geoCohortData = useGeoCohortData()
+    return (
+      geoCohortData.length > 0 &&
+        <GeoCohortMap
+          reportData={geoCohortData}
+          getCursor={getCursor()}
+          fillBasedOn={'Imps'}
           elevationBasedOn={'Revenue'}
           pitch={45}
           mapboxApiAccessToken={mapboxApiAccessToken}
