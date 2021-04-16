30cbbf2b3455e6748130f9d4cab60fbeb10a2523
Erika Szabo <erika.szabo.geo@gmail.com>
2021-04-15 22:32:27 -0400

    Legend - styled legend by adding LegendTitle and LegendElements comps; move label into LegendTitle, above the other elements

--- src/components/legend/index.js
+++ src/components/legend/index.js
@@ -11,14 +11,24 @@ setup(React.createElement)
 const LegendContainer = styled('div')(({ num_legends, position, typography }) => ({
   ...typography,
   display: 'flex',
+  flexDirection: 'column',
   position: 'absolute',
-  zIndex: 9998,
   backgroundColor: 'rgba(255,255,255,0.9)',
   padding: '1rem',
   borderRadius: '0.2rem',
   cursor: num_legends > 1 ? 'pointer' : 'default',
   ...position,
 }))
+
+const LegendTitle = styled('div')`
+  margin-bottom: 10px;
+  fontWeight: bold;
+`
+
+const LegendElements = styled('div')`
+  display: flex;
+  flex-direction: row;
+`
 
 const LegendTextContainer = styled('div')`
   display: flex;
@@ -70,13 +80,16 @@ const Legend = ({ position, legends, typography }) => {
           position={objPosition}
           typography={typography}
         >
-          <LegendSymbolContainer>
-            <LegendSymbol {...symbolProps} />
-          </LegendSymbolContainer>
-          <LegendTextContainer>
-            <LegendText legend-text-top={ top }>{max.toLocaleString()} {label}</LegendText>
-            <LegendText>{min.toLocaleString()} {label}</LegendText>
-          </LegendTextContainer>
+          <LegendTitle>{label}</LegendTitle>
+          <LegendElements>
+            <LegendSymbolContainer>
+              <LegendSymbol {...symbolProps} />
+            </LegendSymbolContainer>
+            <LegendTextContainer>
+              <LegendText legend-text-top={ top }>{max.toLocaleString()}</LegendText>
+              <LegendText>{min.toLocaleString()}</LegendText>
+            </LegendTextContainer>
+          </LegendElements>
         </LegendContainer>
       )}
     </>
