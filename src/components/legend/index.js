c5a6f4012bd2282d99b346723c4a2b7df8c4badc
Erika Szabo <erika.szabo.geo@gmail.com>
2021-04-23 17:48:46 -0400

    Legend - use LegendItem to map out multiple legends

--- src/components/legend/index.js
+++ src/components/legend/index.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { typographyPropTypes, typographyDefaultProps } from '../../shared/map-props'
 import { styled, setup } from 'goober'
 
-import LegendSymbol from './legend-symbol'
+import LegendItem from './legend-item'
 
 
 setup(React.createElement)
@@ -13,35 +13,9 @@ const LegendContainer = styled('div')(({ num_legends, position, typography }) =>
   display: 'flex',
   flexDirection: 'column',
   position: 'absolute',
-  backgroundColor: 'rgba(255,255,255,0.9)',
-  padding: '1rem',
-  borderRadius: '0.2rem',
   cursor: num_legends > 1 ? 'pointer' : 'default',
   ...position,
 }))
-
-const LegendTitle = styled('div')`
-  margin-bottom: 10px;
-  fontWeight: bold;
-`
-
-const LegendElements = styled('div')`
-  display: flex;
-  flex-direction: row;
-`
-
-const LegendTextContainer = styled('div')`
-  display: flex;
-  flex-direction: column;
-`
-
-const LegendText = styled('div')`
-  ${props => props['legend-text-top'] ? 'flex-grow: 1;' : ''};
-  margin-left: 1rem;
-  color: black;
-`
-
-const LegendSymbolContainer = styled('div')``
 
 const propTypes = {
   position: PropTypes.oneOf(['top-left', 'top-right', 'bottom-left', 'bottom-right']),
@@ -67,31 +41,28 @@ const Legend = ({ position, legends, typography }) => {
   let objPosition = {}
   objPosition[position.split('-')[0]] = '1rem'
   objPosition[position.split('-')[1]] = '1rem'
-  const [activeLegend, setActiveLegend] = useState(0)
-  const handleLegendChange = () => setActiveLegend(o => o === legends.length - 1 ? 0 : o + 1)
-  const { max, min, label, ...symbolProps } = legends[activeLegend] || {}
+  // const [activeLegend, setActiveLegend] = useState(0)
+  // const handleLegendChange = () => setActiveLegend(o => o === legends.length - 1 ? 0 : o + 1)
 
   return (
     <>
-      { max !== undefined && min !== undefined && (
-        <LegendContainer
-          num_legends={legends.length}
-          onClick={handleLegendChange}
-          position={objPosition}
-          typography={typography}
-        >
-          <LegendTitle>{label}</LegendTitle>
-          <LegendElements>
-            <LegendSymbolContainer>
-              <LegendSymbol {...symbolProps} />
-            </LegendSymbolContainer>
-            <LegendTextContainer>
-              <LegendText legend-text-top={ top }>{max.toLocaleString()}</LegendText>
-              <LegendText>{min.toLocaleString()}</LegendText>
-            </LegendTextContainer>
-          </LegendElements>
-        </LegendContainer>
-      )}
+      <LegendContainer
+        num_legends={legends.length}
+        // onClick={handleLegendChange}
+        position={objPosition}
+        typography={typography}
+      >
+        {legends.map(({ max, min, label, type, ...symbolProps }) => (
+          <LegendItem
+            key={type}
+            max={max}
+            min={min}
+            label={label}
+            type={type}
+            symbolProps={{ type, ...symbolProps }}
+          />
+        ))}
+      </LegendContainer>
     </>
   )
 }
