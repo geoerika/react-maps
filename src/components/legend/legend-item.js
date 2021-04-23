9ee628673dde6c1f6dee83b2fcaba071e28fcf4f
Erika Szabo <erika.szabo.geo@gmail.com>
2021-04-23 17:47:44 -0400

    LegendItem -create LegendItem comp

---
+++ src/components/legend/legend-item.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { typographyPropTypes, typographyDefaultProps } from '../../shared/map-props'
+import { styled, setup } from 'goober'
+
+import LegendSymbol from './legend-symbol'
+
+
+setup(React.createElement)
+
+const LegendBody = styled('div')`
+  background-color: rgba(255,255,255,0.9);
+  margin-bottom: 5px;
+  padding: 1rem;
+  border-radius: 0.2rem;
+`
+
+const LegendTitle = styled('div')`
+  margin-bottom: 10px;
+  font-weight: bold;
+`
+
+const LegendElements = styled('div')`
+  display: flex;
+  flex-direction: row;
+`
+
+const LegendTextContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+`
+
+const LegendText = styled('div')`
+  ${props => props['legend-text-top'] ? 'flex-grow: 1;' : ''};
+  margin-left: 1rem;
+  color: black;
+`
+
+const LegendSymbolContainer = styled('div')``
+
+const propTypes = {
+  label: PropTypes.string,
+  max: PropTypes.number,
+  min: PropTypes.number,
+  symbolProps: PropTypes.object,
+}
+
+const defaultProps = {
+  label: '',
+  max: undefined,
+  min: undefined,
+  symbolProps: undefined,
+}
+
+const LegendItem = ({ label, min, max, symbolProps }) => {
+  return (
+    <>
+      {max !== undefined && min !== undefined && (
+        <LegendBody>
+          <LegendTitle>{label}</LegendTitle>
+          <LegendElements>
+            <LegendSymbolContainer>
+              <LegendSymbol {...symbolProps} />
+            </LegendSymbolContainer>
+            <LegendTextContainer>
+              <LegendText legend-text-top={ top }>{max.toLocaleString()}</LegendText>
+              <LegendText>{min.toLocaleString()}</LegendText>
+            </LegendTextContainer>
+          </LegendElements>
+        </LegendBody>
+      )}
+    </>
+  )
+}
+
+LegendItem.propTypes = { ...propTypes, ...typographyPropTypes }
+LegendItem.defaultProps = { ...defaultProps,  ...typographyDefaultProps }
+
+export default LegendItem
