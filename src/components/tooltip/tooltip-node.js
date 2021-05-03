5788ffe4d6e1a265fd838647ca437e3727b8a0b2
Erika Szabo <erika.szabo.geo@gmail.com>
2021-05-03 19:27:33 -0400

    tooltipNode - add formatTooltipTitle prop; change Name comp into Title comp

--- src/components/tooltip/tooltip-node.js
+++ src/components/tooltip/tooltip-node.js
@@ -4,7 +4,7 @@ import { styled, setup } from 'goober'
 
 setup(React.createElement)
 
-const Name = styled('div')`
+const Title = styled('div')`
   margin: 0px;
   fontWeight: 700;
 `
@@ -31,16 +31,17 @@ const Values = styled('div')`
  * tooltipNode - returns a node element with: name, id, and 'key: value' pairs for Tooltip component
  * @param { object } param
  * @param { object } param.tooltipKeys - object of attribute keys for Tooltip component
- * @param { object } param.formatData - object of data formatting for different key values
+ * @param { function } param.formatData - object of data formatting for different key values
+ * @param { function } param.formatTooltipTitle - function to truncate Tooltip title
  * @param { object } param.params - object of deck.gl onHover event
  * @returns { Node } - node element
  */
-const tooltipNode = ({ tooltipKeys, formatData, params }) => {
+const tooltipNode = ({ tooltipKeys, formatData, formatTooltipTitle, params }) => {
   const { name, id, metricKeys, nameAccessor, idAccessor, metricAccessor, metricAliases } = tooltipKeys
   return (
     <>
       {name && nameAccessor(params)?.[name] &&
-        <Name>{nameAccessor(params)[name]}</Name>
+        <Title>{formatTooltipTitle(nameAccessor(params)[name])}</Title>
       }
       {id && idAccessor(params)?.[id] &&
         <Id>{idAccessor(params)[id]}</Id>
