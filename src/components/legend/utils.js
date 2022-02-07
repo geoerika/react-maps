07b172191be99db678a6cb152a63634392d26770
Erika Szabo <erika.szabo.geo@gmail.com>
2022-02-07 11:32:16 -0500

    legend/utils - remove conditions for max > 0 as the min and max are now adjusted to consider negative data

--- src/components/legend/utils.js
+++ src/components/legend/utils.js
@@ -19,10 +19,11 @@ export const getLegendItemElements = ({ legendItemProps }) => {
     legendSize,
   } = legendItemProps
 
-  const legendElemWidth = min !== max && max > 0 ?
+  const legendElemWidth = max !== min ?
     LEGEND_SYMBOL_WIDTH[legendSize] :
     LEGEND_SYMBOL_WIDTH.zero
   const title = formatLegendTitle(metricAliases?.[label] || formatPropertyLabel(label))
+
   const [minValue, maxValue] = formatData?.[label] ?
     [formatData[label](min), formatData[label](max)] :
     [min, max]
@@ -71,7 +72,7 @@ export const getLegendItemDimensions = ({
    * text container width for gradient and elevation legends, where value label centres align with
    * the edges of the legend symbol container
    */
-  let textContainerWidth = min !== max && max > 0 && textMinWidth && textMaxWidth ?
+  let textContainerWidth = min !== max && textMinWidth && textMaxWidth ?
     textMinWidth / 2 + textMaxWidth / 2 + legendElemWidth :
     textMinWidth || 0
 
@@ -79,11 +80,11 @@ export const getLegendItemDimensions = ({
   let textContainerLeftMargin = 0
 
   // don't adjust margins when we have no data variance
-  if (min !== max && max > 0 && textMinWidth && type !== LEGEND_TYPE.size) {
+  if (min !== max && textMinWidth && type !== LEGEND_TYPE.size) {
     symbolContainerLeftMargin = textMinWidth / 2
   }
 
-  if (min !== max && max > 0 && textMinWidth && type === LEGEND_TYPE.size) {
+  if (min !== max && textMinWidth && type === LEGEND_TYPE.size) {
     // for radius (size) width is samller, as the value labels align with the centers of the edge circles
     textContainerWidth = !isNaN(size) && size && !isNaN(dots) && dots ?
       textContainerWidth - (2.5 + dots) * size / 2 : // half of each circle size
