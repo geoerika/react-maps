0e4bb466c3ae8005e365c27ba71353660abeb1ca
Erika Szabo <erika.szabo.geo@gmail.com>
2022-06-06 09:39:54 -0400

    utils/string-format - apply toString() to fullStr in truncate function; add decodeXML & getTextSize functions

--- src/utils/string-format.js
+++ src/utils/string-format.js
@@ -1,3 +1,5 @@
+import { FONT_SIZE  } from '../constants'
+
 /**
  * truncate - returns formatted string, by truncating to a certain nr of characters
  * @param { string } fullStr - string to format
@@ -6,7 +8,7 @@
  * @returns { string } - formatted string
  */
 export function truncate(fullStr, strLen, separator = ' ... ') {
-  if (fullStr.length <= strLen) {
+  if (fullStr.toString().length <= strLen) {
     return fullStr
   }
   const sepLen = separator.length
@@ -14,5 +16,60 @@ export function truncate(fullStr, strLen, separator = ' ... ') {
   const frontChars = Math.ceil(charsToShow / 2)
   const endChars = Math.floor(charsToShow / 2)
 
-  return fullStr.substring(0, frontChars) + separator + fullStr.substring(fullStr.length + 1 - endChars)
+  return fullStr.toString()?.substring(0, frontChars) +
+         separator +
+         fullStr.toString()?.substring(fullStr.length + 1 - endChars)
+}
+
+// "vendored" from https://github.com/mdevils/html-entities/blob/68a1a96/src/xml-entities.ts
+const decodeXML = (str) => {
+  const ALPHA_INDEX = {
+    '&lt': '<',
+    '&gt': '>',
+    '&quot': '"',
+    '&apos': '\'',
+    '&amp': '&',
+    '&lt;': '<',
+    '&gt;': '>',
+    '&quot;': '"',
+    '&apos;': '\'',
+    '&amp;': '&',
+  }
+  if (!str || !str.length) {
+    return ''
+  }
+  return str.replace(/&#?[0-9a-zA-Z]+;?/g, function (s) {
+    if (s.charAt(1) === '#') {
+      const code = s.charAt(2).toLowerCase() === 'x' ?
+        parseInt(s.substr(3), 16) :
+        parseInt(s.substr(2))
+
+      if (isNaN(code) || code < -32768 || code > 65535) {
+        return ''
+      }
+      return String.fromCharCode(code)
+    }
+    return ALPHA_INDEX[s] || s
+  })
+}
+
+/**
+ * https://stackoverflow.com/questions/118241/calculate-text-width-with-javascript/50813259#50813259
+ * getTextSize - calculates a rendered text width and height in rem
+ * @param { string } text - a text string
+ * @param { object } typographyProps - an object with font size, font family, and text color for the chart
+ * @returns { object } - the width and height of the rendered text in rem
+ */
+export const getTextSize = (text, fontWeight, fontSize, fontFamily) => {
+  let font = `${fontWeight} ${fontSize}px ${fontFamily}`
+  let canvas = document.createElement('canvas')
+  let context = canvas.getContext('2d')
+  context.font = font
+  let metrics = typeof text === 'number'
+    ? context.measureText(text)
+    : context.measureText(decodeXML(text))
+  return {
+    width: Math.ceil((metrics.width / FONT_SIZE) * 100) / 100,
+    height: (((metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent) / FONT_SIZE) * 100) / 100,
+  }
 }
