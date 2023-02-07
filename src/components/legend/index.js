83fdef0a3bdf058faef863f7cda77d554e5971df
Erika Szabo <erika.szabo.geo@gmail.com>
2023-02-07 14:02:44 -0500

    Legend - adjust layer legend sizing for large layer title names

--- src/components/legend/index.js
+++ src/components/legend/index.js
@@ -1,6 +1,6 @@
 // TODO - make Legend comp more customizable by size, right now it is rigid
 
-import React, { useState } from 'react'
+import React, { useState, useMemo, useRef, forwardRef } from 'react'
 import PropTypes from 'prop-types'
 
 import { getTailwindConfigColor } from '@eqworks/lumen-labs'
@@ -8,7 +8,13 @@ import { styled, setup } from 'goober'
 
 import { typographyPropTypes, typographyDefaultProps } from '../../shared/map-props'
 import LegendItem from './legend-item'
-import { LEGEND_SIZE, LEGEND_POSITION, LEGEND_SYMBOL_WIDTH } from '../../constants'
+import { getElemWidth } from './utils'
+import {
+  LEGEND_SIZE,
+  LEGEND_POSITION,
+  LEGEND_SYMBOL_WIDTH,
+  LEGEND_LAYER_MAX_WIDTH,
+} from '../../constants'
 
 
 setup(React.createElement)
@@ -29,13 +35,13 @@ const LegendContainer = styled('div')(({ num_legends, position, typography, opac
   ...position,
 }))
 
-const LayerTitle = styled('div')`
+const LayerTitle = styled('div', forwardRef)`
   font-weight: 700;
   font-size: 0.75rem;
   text-align: center;
+  maxWidth: ${({ maxwidth }) => maxwidth}rem;
   margin-bottom: 0.275rem;
   margin-left: ${({ titleleftmargin }) => titleleftmargin}rem;
-  maxWidth: ${({ maxwidth }) => maxwidth}rem;
   overflow-wrap: break-word;
 `
 
@@ -49,13 +55,33 @@ const Legend = ({
   const [symbolMarginLeft, setSymbolMarginLeft] = useState(0)
   // the largest offset on the right side of the legend symbol due to underneath text width
   const [rightTextOffset, setRightTextOffset] = useState(0)
+  const [legendItemPadding, setLegendItemPadding] = useState(0)
   const [opacity, setOpacity] = useState(0)
+
+  const layerTitleRef = useRef(null)
 
   let objPosition = {}
   objPosition[legendPosition.split('-')[0]] = '.5rem'
   objPosition[legendPosition.split('-')[1]] = '.5rem'
   // const [activeLegend, setActiveLegend] = useState(0)
   // const handleLegendChange = () => setActiveLegend(o => o === legends.length - 1 ? 0 : o + 1)
+
+  const layerTitleWidth = getElemWidth(layerTitleRef)
+
+  const maxLegendWidth = useMemo(() =>
+    Math.max(
+      LEGEND_LAYER_MAX_WIDTH,
+      symbolMarginLeft + LEGEND_SYMBOL_WIDTH[legendSize] + rightTextOffset,
+    )
+  , [symbolMarginLeft, legendSize, rightTextOffset])
+
+  const layerTitleLeftMargin = useMemo(() => {
+    if (symbolMarginLeft <= (maxLegendWidth - LEGEND_SYMBOL_WIDTH[legendSize]) / 2) {
+      setLegendItemPadding((maxLegendWidth - LEGEND_SYMBOL_WIDTH[legendSize]) / 2 - symbolMarginLeft)
+      return 0
+    }
+    return symbolMarginLeft - layerTitleWidth / 2 +  LEGEND_SYMBOL_WIDTH[legendSize] / 2
+  }, [legendSize, symbolMarginLeft, layerTitleWidth, maxLegendWidth])
 
   return (
     <LegendContainer
@@ -70,8 +96,9 @@ const Legend = ({
         <div key={index}>
           {layerTitle && (
             <LayerTitle
-              titleleftmargin={symbolMarginLeft}
-              maxwidth={LEGEND_SYMBOL_WIDTH[legendSize]}
+              ref={layerTitleRef}
+              titleleftmargin={layerTitleLeftMargin}
+              maxwidth={maxLegendWidth}
             >
               {layerTitle}
             </LayerTitle>
@@ -88,6 +115,7 @@ const Legend = ({
                 setRightTextOffset,
                 setOpacity,
                 ...legendProps,
+                ...{ paddingLeft: legendItemPadding },
               }
             }
           />
